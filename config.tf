b954c92a2829134faadf1d5d4a3f7bbaab346b8e
Luke Carrier <luke@carrier.im>
2018-09-13 10:49:23 +0100

    Set up remote state

--- config.tf
+++ config.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 0.11.7"
+
+  backend "azurerm" {
+    storage_account_name = "avadotftraining0"
+    container_name = "state"
+    key = "prod.tfstate"
+  }
 }
 
 provider "azurerm" {
